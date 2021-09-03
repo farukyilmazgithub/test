ebe2eec4bfcbc3351f6f892f32791e8592f4ba86
farukyilmazgithub <90024205+farukyilmazgithub@users.noreply.github.com>
2021-09-03 09:25:30 +0300

    Add files via upload

---
+++ src/main/java/Interfaces/Item.java
@@ -0,0 +1,11 @@
+package Interfaces;
+
+public class Item implements IDataBase {
+    public void add() {
+    System.out.println("Item added");
+    }
+
+    public void delete() {
+    System.out.println("Item deleted");
+    }
+}
---
+++ src/main/java/Interfaces/Material.java
@@ -0,0 +1,11 @@
+package Interfaces;
+
+public class Material implements IDataBase{
+    public void add() {
+    System.out.println("Material added");
+    }
+
+    public void delete() {
+    System.out.println("Material deleted");
+    }
+}
